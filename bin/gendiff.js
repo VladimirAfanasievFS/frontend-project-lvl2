7fa1c17c4592c2bae60e533d7e7b5d46da35069a
Vova Afan <grmh9ko@gmail.com>
2020-03-08 20:08:44 +0300

    prepare eslint install

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,14 @@ import program from "commander";
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.02.1', '-V, --version', 'output the current version')
+  .option('-f, --format [type]', 'output format', 'defaultValue')
+  .arguments('<firstConfig> <secondConfig>')
+  .action(function (firstConfig, secondConfig) {
+    const firstConfigValue = firstConfig;
+    const secondConfigValue = secondConfig;
+    console.log(firstConfigValue, secondConfigValue);
+  });
+program.parse(process.argv);
 
- program.parse(process.argv);
+if (program.format) console.log(`- ${program.format}`);
 
- 
