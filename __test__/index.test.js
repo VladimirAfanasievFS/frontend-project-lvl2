f711a04d094e402a2ef2bdf74d03a18f60a033ff
Vova Afan <grmh9ko@gmail.com>
2020-03-14 17:18:28 +0300

    stage 9 add GitHub Actions Test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,24 +2,36 @@
 import startGenDiff from '../src/index';
 import readFileData from '../src/modules/readFileData';
 
-const pathFile1 = '__fixtures__/before';
-const pathFile2 = '__fixtures__/after';
-const resultTree = readFileData('__fixtures__/resultTree.txt');
-const resultPlain = readFileData('__fixtures__/resultPlain.txt');
-const resultJSON = readFileData('__fixtures__/resultJSON.txt');
-test('diffJson', () => {
-  expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'tree')).toEqual(resultTree);
-});
-test('diffYaml', () => {
-  expect(startGenDiff(`${pathFile1}.yaml`, `${pathFile2}.yaml`, 'tree')).toEqual(resultTree);
-});
-test('diffIni', () => {
-  expect(startGenDiff(`${pathFile1}.ini`, `${pathFile2}.ini`, 'tree')).toEqual(resultTree);
+
+test.each([
+  ['.json', 'tree'],
+  ['.yaml', 'tree'],
+  ['.ini', 'tree'],
+  ['.json', 'plain'],
+  ['.yaml', 'plain'],
+  ['.ini', 'plain'],
+  ['.json', 'JSON'],
+  ['.yaml', 'JSON'],
+  // ['.ini', 'JSON'],
+])('Files: *%s Format: %s', (extension, format) => {
+  const result = readFileData(`__fixtures__/result${format}.txt`);
+  expect(startGenDiff(`__fixtures__/before${extension}`, `__fixtures__/after${extension}`, format)).toEqual(result);
 });
 
-test('diffJsonPlain', () => {
-  expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'plain')).toEqual(resultPlain);
-});
-test('diffJsonJSON', () => {
-  expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'JSON')).toEqual(resultJSON);
-});
+// test('diffJson', () => {
+  
+//   expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'tree')).toEqual(resultTree);
+// });
+// test('diffYaml', () => {
+//   expect(startGenDiff(`${pathFile1}.yaml`, `${pathFile2}.yaml`, 'tree')).toEqual(resultTree);
+// });
+// test('diffIni', () => {
+//   expect(startGenDiff(`${pathFile1}.ini`, `${pathFile2}.ini`, 'tree')).toEqual(resultTree);
+// });
+
+// test('diffJsonPlain', () => {
+//   expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'plain')).toEqual(resultPlain);
+// });
+// test('diffJsonJSON', () => {
+//   expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'JSON')).toEqual(resultJSON);
+// });
