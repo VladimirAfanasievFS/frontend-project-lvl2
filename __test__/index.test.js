09426243f9210cfddaef8736b55cd4f5114efda1
Vova Afan <grmh9ko@gmail.com>
2020-03-14 17:25:56 +0300

    stage 9 add GitHub Actions Test#2

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -17,21 +17,3 @@ test.each([
   const result = readFileData(`__fixtures__/result${format}.txt`);
   expect(startGenDiff(`__fixtures__/before${extension}`, `__fixtures__/after${extension}`, format)).toEqual(result);
 });
-
-// test('diffJson', () => {
-  
-//   expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'tree')).toEqual(resultTree);
-// });
-// test('diffYaml', () => {
-//   expect(startGenDiff(`${pathFile1}.yaml`, `${pathFile2}.yaml`, 'tree')).toEqual(resultTree);
-// });
-// test('diffIni', () => {
-//   expect(startGenDiff(`${pathFile1}.ini`, `${pathFile2}.ini`, 'tree')).toEqual(resultTree);
-// });
-
-// test('diffJsonPlain', () => {
-//   expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'plain')).toEqual(resultPlain);
-// });
-// test('diffJsonJSON', () => {
-//   expect(startGenDiff(`${pathFile1}.json`, `${pathFile2}.json`, 'JSON')).toEqual(resultJSON);
-// });
