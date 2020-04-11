b62975055a87354d24b2ff14298c2dcbf2d62bc9
Vova Afan <grmh9ko@gmail.com>
2020-04-11 19:08:27 +0300

    rename variable

--- src/formatters/Plain.js
+++ src/formatters/Plain.js
@@ -3,25 +3,24 @@ import _ from 'lodash';
 
 const getStrView = (value) => (_.isObject(value) ? '[complex value]' : value);
 
-const iter = (AST, ancestors = []) => {
+const makePlain = (AST, ancestors = []) => {
   const statusNode = {
     unChanged: () => null,
     added: ({ value }, path) => `Property '${path.join('.')}' was added with value: ${getStrView(value)}`,
     removed: (none, path) => `Property '${path.join('.')}' was deleted`,
     changed: ({ addedValue, removedValue }, path) => (
       `Property '${path.join('.')}' was changed from ${getStrView(removedValue)} to ${getStrView(addedValue)}`),
-    nested: ({ children }, path) => iter(children, path),
+    nested: ({ children }, path) => makePlain(children, path),
   };
 
   const result = AST.map((node) => {
     const { key, status } = node;
     const newAncestors = [...ancestors, key];
-    const retVal = statusNode[status](node, newAncestors);
-    return retVal;
+    return statusNode[status](node, newAncestors);
   });
 
   return _.compact(_.flatten(result)).join('\n');
 };
 
-const renderPlain = iter;
+const renderPlain = makePlain;
 export default renderPlain;
--- src/formatters/Tree.js
+++ src/formatters/Tree.js
@@ -1,15 +1,15 @@
 
 import _ from 'lodash';
 
-const formatSpace = (count, simbol) => `${'    '.repeat(count)}  ${simbol} `;
+const getTab = (count, simbol) => `${'    '.repeat(count)}  ${simbol} `;
 const stringify = (key, obj, depth = 0) => {
   if (!_.isPlainObject(obj)) {
     return `${key}: ${obj}`;
   }
 
   const result = Object.entries(obj).map(([key2, value]) => {
-    const beginSpace = formatSpace(depth + 1, ' ');
-    const endSpace = formatSpace(depth, ' ');
+    const beginSpace = getTab(depth + 1, ' ');
+    const endSpace = getTab(depth, ' ');
     return `${key}: {\n${beginSpace}${key2}: ${value}\n${endSpace}}`;
   });
   return result;
@@ -19,17 +19,17 @@ const makeTree = (AST, depth = 0) => {
   const statusMap = {
     nested: ({ key, children }) => {
       const child = makeTree(children, depth + 1);
-      return `${formatSpace(depth, ' ')}${key}: {\n${child}\n${formatSpace(depth, ' ')}}`;
+      return `${getTab(depth, ' ')}${key}: {\n${child}\n${getTab(depth, ' ')}}`;
     },
     unChanged: ({ key, value }) => {
-      const space = formatSpace(depth, ' ');
+      const space = getTab(depth, ' ');
       return `${space}${stringify(key, value, depth)}`;
     },
-    added: ({ key, value }) => `${formatSpace(depth, '+')}${stringify(key, value, depth)}`,
-    removed: ({ key, value }) => `${formatSpace(depth, '-')}${stringify(key, value, depth)}`,
+    added: ({ key, value }) => `${getTab(depth, '+')}${stringify(key, value, depth)}`,
+    removed: ({ key, value }) => `${getTab(depth, '-')}${stringify(key, value, depth)}`,
     changed: ({ key, addedValue, removedValue }) => {
-      const added = `${formatSpace(depth, '+')}${stringify(key, addedValue, depth)}`;
-      const removed = `${formatSpace(depth, '-')}${stringify(key, removedValue, depth)}`;
+      const added = `${getTab(depth, '+')}${stringify(key, addedValue, depth)}`;
+      const removed = `${getTab(depth, '-')}${stringify(key, removedValue, depth)}`;
       return `${added}\n${removed}`;
     },
   };
