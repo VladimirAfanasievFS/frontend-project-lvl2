246a3b1a8549cc7ebb59390242c4b7e5617b2d5f
Vladimir Afanasiev <vladimir.afanasiev@flatstack.dev>
2022-05-03 23:47:55 +0300

    add default render

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import getRender from './formatters/index.js';
 
 const getExtension = (filePath) => path.extname(filePath).slice(1);
 const parse = (filePath, data) => getParser(getExtension(filePath))(data);
-const genDiff = (path1, path2, typeRender) => {
+const genDiff = (path1, path2, renderName = 'stylish') => {
   const data1 = readFileData(path1);
   const data2 = readFileData(path2);
   const structuredData1 = parse(path1, data1);
   const structuredData2 = parse(path2, data2);
   const difference = generateDiff(structuredData1, structuredData2);
-  const render = getRender(typeRender);
+  const render = getRender(renderName);
   return render(difference);
 };
 
