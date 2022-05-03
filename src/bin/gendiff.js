a91b4f0da08888dc5278648e1853d0c640b09f87
Vladimir Afanasiev <vladimir.afanasiev@flatstack.dev>
2022-05-03 23:51:42 +0300

    fix linter

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,17 +2,12 @@
 import program from 'commander';
 import genDiff from '../index.js';
 
-let pathToFile1;
-let pathToFile2;
-
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.02.1', '-V, --version', 'output the current version')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<firstConfig> <secondConfig>')
-  .action((argPathToFile1, argPathToFile2) => {
-    pathToFile1 = argPathToFile1;
-    pathToFile2 = argPathToFile2;
+  .action((pathToFile1, pathToFile2) => {
     console.log(genDiff(pathToFile1, pathToFile2, program.format));
   });
 program.parse(process.argv);
